b8d9b256cc0dbf6efc2cea4aeed05159f98a9b84
dsinj <dsinjiashvili@gmail.com>
2020-10-04 14:02:54 -0500

    API models and test

--- src/test/java/petStoreTestApi/CreatePet.java
+++ src/test/java/petStoreTestApi/CreatePet.java
@@ -3,6 +3,7 @@ package petStoreTestApi;
 import helper.TestDataGenerator;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import models.Category;
 import models.PetRequest;
 import models.PetResponse;
 import org.testng.Assert;
@@ -39,12 +40,29 @@ public class CreatePet {
                 .extract()
                 .response();
         JsonPath jsonpath = response.jsonPath();
+        // $ means all object
         PetResponse petResponse = jsonpath.getObject("$",PetResponse.class);
         //System.out.println(petResponse.getId());
         petId = petResponse.getId();
         Assert.assertEquals(petRequest.getStatus(),petResponse.getStatus());
         Assert.assertEquals(petRequest.getName(),petResponse.getName());
         Assert.assertEquals(petRequest.getCategory().getName(),petResponse.getCategory().getName());
+
+
+        // By selected object
+        // Example 1
+//        Category categoryResponse = jsonpath.getObject("category",Category.class);
+//        System.out.println(categoryResponse.getName());
+
+        // Example 2
+//        String petstatusResponse = jsonpath.getString("status");
+//        System.out.println(petstatusResponse);
+
+        // Example 3
+//        long id = jsonpath.getLong("id");
+//        System.out.println(id);
+
+
 
     }
 }
